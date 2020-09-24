d27f298c21797a14e965b371b0c5be5d959e68a9
Ricardo Marquez <rikmarqk@gmail.com>
2020-09-24 06:57:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-programmer_hash[:dennis_ritchie][:languages][0].to_s
+puts programmer_hash[:dennis_ritchie][:languages][0].to_s
 end
 
 def adding_matz
